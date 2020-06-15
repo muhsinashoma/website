8d1b425f5e2af05b8571af69a8a6d912a2d68b13
muhsina.akter <muhsina.akter2@gmail.com>
2020-06-15 12:12:42 +0600

    Bootstrap carousel and animated css done

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <meta charset="utf-8">
+      <title>Document</title>
+  </head>
+
+  <style>
+      link href="assets/css/style.css" rel="stylesheet">
+  </style>
+
+  <body class="wrap-table">
+
+  <h1>We are Developers</h1>
+  </body>
+
+</html>
